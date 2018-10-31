3b7b8675c92ecbaf4052ea0570974dfc43dc5453
Urvish Patel <urvishp13@gmail.com>
2018-10-31 11:08:42 -0400

    finished scaffolding

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("sriracha", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheese burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("regular", false);
